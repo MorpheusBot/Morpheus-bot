5d90e4b3646bdeec995b7bf82384151c0be0fda9
Solumath <solumath99@gmail.com>
2024-08-25 10:22:08 +0200

    remove git pull

--- cogs/system/cog.py
+++ cogs/system/cog.py
@@ -3,11 +3,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import discord
-import git
 from discord import app_commands
 from discord.ext import commands
 
-import utils.utils as utils
 from cogs.base import Base
 from custom.cooldowns import default_cooldown
 from custom.permission_check import is_bot_admin
@@ -21,41 +19,11 @@ if TYPE_CHECKING:
     from morpheus import Morpheus
 
 
-class Git:
-    def __init__(self):
-        self.repo = git.Repo(search_parent_directories=True)
-        self.cmd = self.repo.git
-
-    async def pull(self):
-        return self.cmd.pull()
-
-    def short_hash(self):
-        return self.repo.head.object.hexsha[:7]
-
-
 class System(Base, commands.Cog):
     def __init__(self, bot: Morpheus):
         super().__init__()
         self.bot = bot
-        self.git = Git()
-
         self.unloadable_cogs = ["system"]
-
-    git_group = app_commands.Group(name="git", description=SystemMess.git_brief)
-
-    @app_commands.check(is_bot_admin)
-    @git_group.command(name="pull", description=SystemMess.git_pull_brief)
-    async def pull(self, inter: discord.Interaction):
-        await inter.response.send_message("`Pulling`")
-        message = await inter.original_response()
-
-        pull_result = await self.git.pull()
-        pull_parts = utils.cut_string(pull_result, 1900)
-
-        await message.edit(content=f"```{pull_parts[0]}```")
-
-        for part in pull_parts[1:]:
-            await inter.followup.send(f"```{part}```")
 
     @app_commands.check(is_bot_admin)
     @app_commands.command(name="shutdown", description=SystemMess.shutdown_brief)
