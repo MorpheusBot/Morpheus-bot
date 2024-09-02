54531617e0bdbedcc0f6910a1e1c311152a8bc5b
Solumath <solumath99@gmail.com>
2024-09-02 10:56:38 +0200

    fix: missing init_db

--- database/init_db.py
+++ database/init_db.py
@@ -4,3 +4,10 @@ File to initialize/drop the database.
 
 # this is to import all models so that they are recognized by Alembic
 from database import *  # noqa
+from database.database import Base, database
+
+
+async def init_db():
+    async with database.engine.begin() as conn:
+        # await conn.run_sync(database.base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
